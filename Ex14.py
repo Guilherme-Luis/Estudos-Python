998d03d40f5c3d669f3617820e2935d51078dedb
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2024-05-17 22:27:49 -0300

    (17/05) correction exercises

--- Ex14.py
+++ Ex14.py
@@ -4,4 +4,4 @@ if (X > 50):
     Z = (Y * 4.0)
     print(f"The penaly will cost: R${Z}")
 else:
-    print(f"You don't need to pay the penaly")
+    print("You don't need to pay the penaly")
--- Ex16.py
+++ Ex16.py
@@ -3,8 +3,8 @@ X = float(input("Enter with meter(m²): "))
 Y = (X/3)
 
 if (Y <= 18):
-    print(f"You have to buy one can of paint")
-    print(f"So, you have to spend R$80.00")
+    print("You have to buy one can of paint")
+    print("So, you have to spend R$80.00")
 else:
     Z = (Y / 18)
     print(f"You have to buy {ceil(Z)} can of pain")
